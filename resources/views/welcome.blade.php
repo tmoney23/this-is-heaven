dbf0574c791dd6443edd9c2a13e92b9827d41770
DrewRoberts <drew@roberts.email>
2018-01-06 16:57:20 -0500

    Update Hack

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,8 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Google
-                    <input class="gsfi" id="lst-ib" maxlength="2048" name="q" autocomplete="off" title="Search" type="text" value="" aria-label="Search" aria-haspopup="false" role="combobox" aria-autocomplete="list" dir="ltr" spellcheck="false" style="border: none; padding: 0px; margin: 0px; height: auto; width: 100%; background: url(&quot;data:image/gif;base64,R0lGODlhAQABAID/AMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw%3D%3D&quot;) transparent; position: absolute; z-index: 6; left: 0px; outline: none;">
+                    Never One For Words
                 </div>
 
                 <div class="links">
