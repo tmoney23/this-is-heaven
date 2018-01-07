c1628bd00dc129d14f2b8c5414c16af8665c6b57
tmoney23 <thomasdmoore@email.com>
2018-01-06 23:35:06 -0500

    update chg clr  updates

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -71,7 +71,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    well oh right.
+                    my world is rocked.
                 </div>
             </div>
         </div>
