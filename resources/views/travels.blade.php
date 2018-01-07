f7c4f210be0cd29ef6d18c67b416c522fe63eb59
tmoney23 <thomasdmoore@email.com>
2018-01-07 09:52:07 -0500

    create about page3

--- resources/views/travels.blade.php
+++ resources/views/travels.blade.php
@@ -62,34 +62,10 @@
             <div class="content">
                 <div class="title m-b-md">
                     ramblin' man
-                    <div class="card-group">
-                      <div class="card">
-                        <img class="church.jpeg" src="..." alt="Card image cap">
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                      </div>
-                      <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                      </div>
-                      <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                      </div>
-                    </div>
+
                 </div>
-                <img src="/tmoney.jpg" width="30%" height="10%"/>
+                <img src="/tmoney.jpg" width="10%" height="10%"/>
+                <img src="/midwayhorses.jpg" width="10%" height="10%"/>
             </div>
         </div>
     </body>
