21be3dd6b151246877bdbe31f453ae3831b959a1
tmoney23 <thomasdmoore@email.com>
2018-01-07 09:49:49 -0500

    create about page2

--- resources/views/travels.blade.php
+++ resources/views/travels.blade.php
@@ -64,7 +64,7 @@
                     ramblin' man
                     <div class="card-group">
                       <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <img class="church.jpeg" src="..." alt="Card image cap">
                         <div class="card-body">
                           <h5 class="card-title">Card title</h5>
                           <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
