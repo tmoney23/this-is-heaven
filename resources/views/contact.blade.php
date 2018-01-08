4be8b15e00a53ddf6df6b2857abcd121d3dfc40a
tmoney23 <thomasdmoore@email.com>
2018-01-07 22:20:33 -0500

    edit contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,56 +13,56 @@
     </head>
     <body>
       <section class="hero is-success is-fullheight has-bg-img">
-  <!-- Hero head: will stick at the top -->
-  <div class="hero-head">
-    <header class="navbar">
-      <div class="container">
-        <div class="navbar-brand">
-          <a href="/" class="navbar-item">
-            <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
-          </a>
-          <span class="navbar-burger burger" data-target="navbarMenuHeroC">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-        </div>
-        <div id="navbarMenuHeroC" class="navbar-menu">
-          <div class="navbar-end">
-            <a href="/nouns" class="navbar-item is-active">nouns</a>
-            <a href="/travels" class="navbar-item">travels</a>
-            <a href="/prints" class="navbar-item">prints</a>
-            <span class="navbar-item">
-              <a href="https://instagram.com/thomasdmoore" class="button is-success is-inverted">
-                <span class="icon">
-                  <i class="fa fa-instagram"></i>
+        <!-- Hero head: will stick at the top -->
+        <div class="hero-head">
+          <header class="navbar">
+            <div class="container">
+              <div class="navbar-brand">
+                <a href="/" class="navbar-item">
+                  <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+                </a>
+                <span class="navbar-burger burger" data-target="navbarMenuHeroC">
+                  <span></span>
+                  <span></span>
+                  <span></span>
                 </span>
-                <span>thomasdmoore</span>
-              </a>
-            </span>
-          </div>
+              </div>
+              <div id="navbarMenuHeroC" class="navbar-menu">
+                <div class="navbar-end">
+                  <a href="/nouns" class="navbar-item is-active">nouns</a>
+                  <a href="/travels" class="navbar-item">travels</a>
+                  <a href="/prints" class="navbar-item">prints</a>
+                  <span class="navbar-item">
+                    <a href="https://instagram.com/thomasdmoore" class="button is-success is-inverted">
+                      <span class="icon">
+                        <i class="fa fa-instagram"></i>
+                      </span>
+                      <span>thomasdmoore</span>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </header>
         </div>
-      </div>
-    </header>
-  </div>
 
 
-  <!-- Hero footer: will stick at the bottom -->
-  <div class="hero-foot">
-    <nav class="tabs is-boxed is-fullwidth">
-      <div class="container">
-        <ul>
-          <li class="is-active"><a>Overview</a></li>
-          <li><a>Modifiers</a></li>
-          <li><a>Grid</a></li>
-          <li><a>Elements</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-</section>
+        <!-- Hero footer: will stick at the bottom -->
+        <div class="hero-foot">
+          <nav class="tabs is-boxed is-fullwidth">
+            <div class="container">
+              <ul>
+                <li class="is-active"><a>Overview</a></li>
+                <li><a>Modifiers</a></li>
+                <li><a>Grid</a></li>
+                <li><a>Elements</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </section>
 
         <div class="flex-center position-ref full-height">
 
