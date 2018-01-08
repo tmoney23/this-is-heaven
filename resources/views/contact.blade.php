630175a0475edb7fcb54c1708003de0ada091684
tmoney23 <thomasdmoore@email.com>
2018-01-07 22:16:28 -0500

    edit contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-      <section class="hero is-success is-fullheight">
+      <section class="hero is-success is-fullheight has-bg-img">
   <!-- Hero head: will stick at the top -->
   <div class="hero-head">
     <header class="navbar">
