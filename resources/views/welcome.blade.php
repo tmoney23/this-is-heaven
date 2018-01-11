548a1d4dade08e2af0426953ef2fac7eb0890966
tmoney23 <thomasdmoore@email.com>
2018-01-11 01:20:13 -0500

    thomasdmoore edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,29 +5,53 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Never One For Words</title>
+        <title>Contact | Never One For Words</title>
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:100,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="/css/app.css">
-        <link rel="stylesheet" href="/css/loading.css">
     </head>
     <body>
+      <section class="hero is-success is-fullheight bg-img-tmoney">
+        <!-- Hero head: will stick at the top -->
+        <div class="hero-head">
+          <header class="navbar">
+            <div class="container">
+              <div class="navbar-brand">
+                <a href="/" class="navbar-item">
+                  <span>thomas darnell moore | photography</span>
+
+              </div>
+              <div id="navbarMenuHeroC" class="navbar-menu">
+                <div class="navbar-end">
+                  <a href="/nouns" class="navbar-item is-active">nouns</a>
+                  <a href="/travels" class="navbar-item">travels</a>
+                  <a href="/prints" class="navbar-item">prints</a>
+                  <span class="navbar-item">
+                    <a href="/contact" class="button is-success is-inverted">
+                      <span class="icon">
+                        <i class="fa fa-paper-plane"></i>
+                      </span>
+                      <span>contact</span>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
+
+
+
+      </section>
 
         <div class="flex-center position-ref full-height">
 
             <div class="content">
                 <div class="title m-b-md">
-                    never one for words
-                </div>
-
-                <div class="links">
-                    <a href="https://instagram.com/thomasdmoore">instagram</a>
-                    <a href="/nouns">nouns</a>
-                    <a href="/travels">travels</a>
-                    <a href="/about">about</a>
-                    <a href="/contact">contact</a>
                 </div>
             </div>
+
         </div>
     </body>
 </html>
