be433ac3ef55a7deda9c851b531d0f16593cd5a3
tmoney23 <thomasdmoore@email.com>
2018-01-11 13:50:25 -0500

    thomasdmoore edit

--- resources/views/nouns.blade.php
+++ resources/views/nouns.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Contact | Never One For Words</title>
+        <title>nouns | tdm photography</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:100,600" rel="stylesheet" type="text/css">
@@ -18,7 +18,7 @@
           <header class="navbar">
             <div class="container">
               <div class="navbar-brand">
-                <a href="/" class="navbar-item">
+                <a href="/nouns" class="navbar-item">
                   <span>thomas darnell moore | photography</span>
 
               </div>
--- resources/views/travels.blade.php
+++ resources/views/travels.blade.php
@@ -5,21 +5,53 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Travels | Never One For Words</title>
+        <title>travels | tdm photography</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:100,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
+      <section>
+        <!-- Hero head: will stick at the top -->
+        <div class="hero-head">
+          <header class="navbar">
+            <div class="container">
+              <div class="navbar-brand">
+                <a href="/travels" class="navbar-item">
+                  <span>thomas darnell moore | photography</span>
+
+              </div>
+              <div id="navbarMenuHeroC" class="navbar-menu">
+                <div class="navbar-end">
+                  <a href="/nouns" class="navbar-item is-active">nouns</a>
+                  <a href="/travels" class="navbar-item">travels</a>
+                  <a href="/prints" class="navbar-item">prints</a>
+                  <span class="navbar-item">
+                    <a href="/contact" class="button is-success is-inverted">
+                      <span class="icon">
+                        <i class="fa fa-paper-plane"></i>
+                      </span>
+                      <span>contact</span>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
+
+
+
+      </section>
+
         <div class="flex-center position-ref full-height">
 
             <div class="content">
                 <div class="title m-b-md">
-
-
                 </div>
             </div>
+
         </div>
     </body>
 </html>
