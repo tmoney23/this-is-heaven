32913d28aa75036c10cda8afc21c0849d0993c2f
tmoney23 <thomasdmoore@email.com>
2018-01-11 10:29:19 -0500

    thomasdmoore edit

--- resources/views/nouns.blade.php
+++ resources/views/nouns.blade.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-      <section class="hero is-success is-fullheight bg-img-northernlights">
+      <section class="hero is-success is-fullheight bg-img-northerslights">
         <!-- Hero head: will stick at the top -->
         <div class="hero-head">
           <header class="navbar">
