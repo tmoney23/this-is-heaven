8ddad71f78920a75d9decb89df4f6b12f88fcaa0
tmoney23 <thomasdmoore@email.com>
2018-01-10 22:55:31 -0500

    thomasdmoore edit

--- resources/views/nouns.blade.php
+++ resources/views/nouns.blade.php
@@ -5,21 +5,51 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>About | Never One For Words</title>
+        <title>Contact | Never One For Words</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:100,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
+      <section class="hero is-success is-fullheight bg-img-midwayhorses">
+        <!-- Hero head: will stick at the top -->
+        <div class="hero-head">
+          <header class="navbar">
+            <div class="container">
+              <div class="navbar-brand">
+                  <span>thomas darnell moore | photography</span>
+              </div>
+              <div id="navbarMenuHeroC" class="navbar-menu">
+                <div class="navbar-end">
+                  <a href="/nouns" class="navbar-item is-active">nouns</a>
+                  <a href="/travel journal" class="navbar-item">travels</a>
+                  <a href="/prints" class="navbar-item">prints</a>
+                  <span class="navbar-item">
+                    <a href="/contact" class="button is-success is-inverted">
+                      <span class="icon">
+                        <i class="fa fa-paper-plane"></i>
+                      </span>
+                      <span>contact</span>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
+
+
+
+      </section>
+
         <div class="flex-center position-ref full-height">
 
             <div class="content">
                 <div class="title m-b-md">
-                    persons, places, & things
                 </div>
-                <img src="/tmoney.jpg" width="30%" height="10%"/>
             </div>
+
         </div>
     </body>
 </html>
