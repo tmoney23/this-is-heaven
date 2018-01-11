dd962d512208cef1145aee9b9a426b55712fd209
tmoney23 <thomasdmoore@email.com>
2018-01-11 12:24:24 -0500

    thomasdmoore edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,57 +1,83 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Contact | Never One For Words</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:100,600" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="/css/app.css">
-    </head>
-    <body>
-      <section>
-        <!-- Hero head: will stick at the top -->
-        <div class="hero-head">
-          <header class="navbar">
-            <div class="container">
-              <div class="navbar-brand">
-                <a href="/" class="navbar-item">
-                  <span>thomas darnell moore | photography</span>
-
-              </div>
-              <div id="navbarMenuHeroC" class="navbar-menu">
-                <div class="navbar-end">
-                  <a href="/nouns" class="navbar-item is-active">nouns</a>
-                  <a href="/travels" class="navbar-item">travels</a>
-                  <a href="/prints" class="navbar-item">prints</a>
-                  <span class="navbar-item">
-                    <a href="/contact" class="button is-success is-inverted">
-                      <span class="icon">
-                        <i class="fa fa-paper-plane"></i>
-                      </span>
-                      <span>contact</span>
-                    </a>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Landing - thomas</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+  <!-- Bulma Version 0.6.0 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css" integrity="sha256-HEtF7HLJZSC3Le1HcsWbz1hDYFPZCqDhZa9QsCgVUdw=" crossorigin="anonymous" />
+  <link rel="stylesheet" type="text/css" href="../css/landing.css">
+</head>
+<body>
+  <section class="hero is-info is-fullheight">
+    <div class="hero-head">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="../">
+              <img src="'/img/black-church.jpg'" alt="Logo">
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenu">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div id="navbarMenu" class="navbar-menu">
+            <div class="navbar-end">
+              <a class="navbar-item is-active">
+                Home
+              </a>
+              <a class="navbar-item">
+                Examples
+              </a>
+              <a class="navbar-item">
+                Documentation
+              </a>
+              <span class="navbar-item">
+                <a class="button is-white is-outlined is-small" href="https://github.com/dansup/bulma-templates/blob/master/templates/landing.html">
+                  <span class="icon">
+                    <i class="fa fa-github"></i>
                   </span>
-                </div>
-              </div>
+                  <span>View Source</span>
+                </a>
+              </span>
             </div>
-          </header>
+          </div>
         </div>
+      </nav>
+    </div>
 
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="column is-6 is-offset-3">
+          <h1 class="title">
+            Coming Soon
+          </h1>
+          <h2 class="subtitle">
+             $this is the best software platform for running an internet business. We handle billions of dollars every year for forward-thinking businesses around the world.
+          </h2>
+          <div class="box">
 
-
-      </section>
-
-        <div class="flex-center position-ref full-height">
-
-            <div class="content">
-                <div class="title m-b-md">
-                </div>
+            <div class="field is-grouped">
+              <p class="control is-expanded">
+                <input class="input" type="text" placeholder="Enter your email">
+              </p>
+              <p class="control">
+                <a class="button is-info">
+                  Notify Me
+                </a>
+              </p>
             </div>
-
+          </div>
         </div>
-    </body>
+      </div>
+    </div>
+
+  </section>
+  <script async type="text/javascript" src="../js/bulma.js"></script>
+</body>
 </html>
