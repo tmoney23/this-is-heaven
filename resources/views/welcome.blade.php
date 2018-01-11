4ebec7682cf85eabc5bba89f91e11a44a1cd60a9
tmoney23 <thomasdmoore@email.com>
2018-01-11 14:39:46 -0500

    thomasdmoore edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <div class="container is-fullhd">
+  <div class="container is-fulllength">
   <div class="notification">
       <span>thomas darnell moore | photography</span>
   </div>
