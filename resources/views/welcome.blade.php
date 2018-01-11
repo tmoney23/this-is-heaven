9bc4f9387cef584528b3d7f79842ebdbbc078736
tmoney23 <thomasdmoore@email.com>
2018-01-11 13:15:55 -0500

    thomasdmoore edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
   <link rel="stylesheet" type="text/css" href="../css/landing.css">
 </head>
 <body>
-  <section class="hero is-success is-fullheight bg-img-northerslights">
+  <section class="hero is-success is-fullheight bg-img-northernlights">
     <!-- Hero head: will stick at the top -->
     <div class="hero-head">
       <header class="navbar">
