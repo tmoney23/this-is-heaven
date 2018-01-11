bd02801ecdf7bff332f082effc9e29a2bba0970d
tmoney23 <thomasdmoore@email.com>
2018-01-11 11:56:33 -0500

    thomasdmoore edit

--- resources/views/prints.blade.php
+++ resources/views/prints.blade.php
@@ -63,7 +63,7 @@
             <div class="content">
                 <div class="title m-b-md">
                 </div>
-                <img src="/img/tmoney.jpg" width="30%" height="10%"/>
+                <img src="/img/church.jpg" width="100%" height="100%"/>
             </div>
         </div>
     </body>
