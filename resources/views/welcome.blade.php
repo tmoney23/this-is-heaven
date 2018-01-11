d68046e75dfc60f6ab95eb4a3f26ba6dc7f71058
tmoney23 <thomasdmoore@email.com>
2018-01-11 14:56:58 -0500

    thomasdmoore edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,7 @@
     <div class="content has-text-centered">
       <p>
         <strong>Bulma</strong> by <a href="https://jgthms.com">Thomas Moore</a>
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+        <a href="http://opensource.org/licenses/mit-license.php">hsl(0, 0%, 4%)</a>. The website content
         is licensed to hahn media <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
       </p>
     </div>
