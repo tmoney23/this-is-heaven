d401ec52d978cd3699f461ba288c7544f5c905b3
tmoney23 <thomasdmoore@email.com>
2018-01-11 12:05:41 -0500

    thomasdmoore edit

--- resources/views/prints.blade.php
+++ resources/views/prints.blade.php
@@ -63,7 +63,7 @@
             <div class="content">
                 <div class="title m-b-md">
                 </div>
-                <img src="/img/hotpool.jpg" width="50%" height="20%"/>
+                <img src="/img/icelandroad.jpg" width="100%" height="50%"/>
             </div>
         </div>
     </body>
