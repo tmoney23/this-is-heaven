b6dc6bf1a9cd38c3e2cc60625c3a89d53f990c07
tmoney23 <thomasdmoore@email.com>
2018-01-11 20:01:12 -0500

    thomasdmoore edit

--- resources/views/prints.blade.php
+++ resources/views/prints.blade.php
@@ -87,7 +87,7 @@
         <div class="content">
             <div class="title m-b-md">
             </div>
-            <img src="/img/kotormontenegro.jpg" width="50%" height="40%"/><span><img src="/img/tobacco-barn.jpg" width="50%" height="40%"/>
+            <img src="/img/kotormontenegro.jpg" width="50%" height="40%"/><img src="/img/tobaccoo-barn.jpg" width="50%" height="40%"/>
         </div>
     </div>
 </body>
