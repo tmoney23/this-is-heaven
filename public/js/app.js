8079a8fcd88e7df9653bf2bb5d73670e7a26252f
tmoney23 <thomasdmoore@email.com>
2018-01-11 20:38:58 -0500

    thomasdmoore edit

--- public/js/app.js
+++ public/js/app.js
@@ -43041,7 +43041,7 @@ module.exports = Component.exports
 /***/ "./resources/assets/sass/app.scss":
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n$grey-lighter\n^\n      Expected ':' after $grey-lighter in assignment statement\n      in /Users/thomasmoore/Code/neveroneforwords/resources/assets/sass/_variables.scss (line 53, column 1)\n    at runLoaders (/Users/thomasmoore/Code/neveroneforwords/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/thomasmoore/Code/neveroneforwords/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/thomasmoore/Code/neveroneforwords/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/thomasmoore/Code/neveroneforwords/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/thomasmoore/Code/neveroneforwords/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.<anonymous> (/Users/thomasmoore/Code/neveroneforwords/node_modules/async/dist/async.js:2257:31)\n    at Object.callback (/Users/thomasmoore/Code/neveroneforwords/node_modules/async/dist/async.js:958:16)\n    at options.error (/Users/thomasmoore/Code/neveroneforwords/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
