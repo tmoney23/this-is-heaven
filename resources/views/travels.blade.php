49c32c0dda00064a929d9436b9fdcfb2e7945499
tmoney23 <thomasdmoore@email.com>
2018-01-18 12:07:17 -0500

    travel pg

--- resources/views/travels.blade.php
+++ resources/views/travels.blade.php
@@ -80,8 +80,8 @@
 
   </div>
     <div class="hero-body">
-      <div class="container has-text-centered">
-        <div class="columns is-left">
+      <div class="container has-text-left">
+        <div class="columns is-offset">
           <div class="column is-5">
             <figure class="image is-4by3">
               <img src="/img/midwayhorses.jpg" alt="hey">
