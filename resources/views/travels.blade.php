1b878ab04fe90d0328be712e83dac2b72e0c05d9
tmoney23 <thomasdmoore@email.com>
2018-01-18 16:12:43 -0500

    travel pg

--- resources/views/travels.blade.php
+++ resources/views/travels.blade.php
@@ -85,17 +85,14 @@
               <img src="/img/midwayhorses.jpg" alt="hey">
             </figure>
           </div>
-          <div class="column is-4 is-left-1">
-            <h1 class="title is-4">
+          <div class="column is-2 is-left-1">
+            <h1 class="title is-2">
               Horse Farms in Midway, KY
             </h1>
-            <h2 class="subtitle is-4 is-left-3">
+            <h2 class="title is-2 is-left-2">
               12.15.2017
             </h2>
             <br>
-            <p class="has-text-centered">
-              <a class="button is-medium is-info is-outlined">
-                12.15.2018
               </a>
             </p>
           </div>
