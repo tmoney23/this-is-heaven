5270de3d1c0521d88f6b49df45658afa6908e22a
tmoney23 <thomasdmoore@email.com>
2018-01-18 02:03:44 -0500

    travel pg

--- resources/views/travels.blade.php
+++ resources/views/travels.blade.php
@@ -78,6 +78,33 @@
       </div>
     </div>
 
+  </div>
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="columns is-vcentered">
+          <div class="column is-5">
+            <figure class="image is-4by3">
+              <img src="/img/midwayhorses.jpg" alt="hey">
+            </figure>
+          </div>
+          <div class="column is-6 is-offset-1">
+            <h1 class="title is-4">
+              Midway, KY
+            </h1>
+            <h2 class="subtitle is-4">
+              12.15.2017
+            </h2>
+            <br>
+            <p class="has-text-centered">
+              <a class="button is-medium is-info is-outlined">
+                Learn more
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="hero-foot">
       <div class="container">
         <div class="tabs is-centered">
