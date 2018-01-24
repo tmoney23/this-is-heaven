d83e155a99beb05dc69d1890c288931f4f6ed259
tmoney23 <thomasdmoore@email.com>
2018-01-23 20:06:11 -0500

    travel pg

--- resources/views/nouns.blade.php
+++ resources/views/nouns.blade.php
@@ -194,7 +194,7 @@
         <div class="column is-3">
           <div class="panel">
             <p class="is-marginless">
-              <img src="https://placehold.it/300x300">
+              <img src="/img/church.jpg">
             </p>
             <div class="panel-block">
               <div class="columns">
