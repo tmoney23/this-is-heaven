61388c496b577ec318aac2689d8d353ba50c12bf
tmoney23 <thomasdmoore@email.com>
2018-01-23 19:50:02 -0500

    travel pg

--- resources/views/nouns.blade.php
+++ resources/views/nouns.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Grid Gallery - Free Bulma template</title>
+  <title>thomas darnell moore | photos</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.1.0/css/bulma.min.css">
   <link rel="stylesheet" type="text/css" href="../css/grid-gallery.css">
@@ -44,7 +44,7 @@
           <a class="button">
             Log in
           </a>
-          <a class="button is-info">
+          <a class="button is-info is-success is-inverted">
             Sign up
           </a>
         </span>
