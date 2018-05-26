a812e672a8a58373909ddab223af3f73c52b6abb
tmoney23 <thomasdmoore@email.com>
2018-05-26 18:27:21 -0400

    index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,7 +61,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Home are cool</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
