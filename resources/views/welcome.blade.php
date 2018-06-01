3dca837629096e4a2faca2cd1fdb61100d5c8745
tmoney23 <thomasdmoore@email.com>
2018-06-01 16:29:26 -0400

    new links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,173 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- ========== Meta Tags ========== -->
+    <meta charset="UTF-8">
+    <meta name="description" content="Cocoon -Portfolio">
+    <meta name="keywords" content="Cocoon , Portfolio">
+    <meta name="author" content="Pharaohlab">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- ========== Title ========== -->
+    <title> Cocoon -Portfolio</title>
+    <!-- ========== Favicon Ico ========== -->
+    <!--<link rel="icon" href="fav.ico">-->
+    <!-- ========== STYLESHEETS ========== -->
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Fonts Icon CSS -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/et-line.css" rel="stylesheet">
+    <link href="assets/css/ionicons.min.css" rel="stylesheet">
+    <!-- Carousel CSS -->
+    <link href="assets/css/slick.css" rel="stylesheet">
+    <!-- Magnific-popup -->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+</head>
+<body>
+<div class="loader">
+    <div class="loader-outter"></div>
+    <div class="loader-inner"></div>
+</div>
 
-body {
-	font-family: 'Open Sans', sans-serif;
-}
+<div class="body-container container-fluid">
+    <a class="menu-btn" href="javascript:void(0)">
+        <i class="ion ion-grid"></i>
+    </a>
+    <div class="row justify-content-center">
+        <!--=================== side menu ====================-->
+        <div class="col-lg-2 col-md-3 col-12 menu_block">
 
-form {
-	max-width: 500px;
-	margin: 0 auto;
-}
+            <!--logo -->
+            <div class="logo_box">
+                <a href="#">
+                    <img src="assets/img/logo.png" alt="cocoon">
+                </a>
+            </div>
+            <!--logo end-->
 
-button {
-  color: #777;
-	background: #fff;
-  padding: 4px 12px 4px 12px;
-  border: solid #777 2px;
-	transition: all 0.3s ease-in-out;
-  text-decoration: none;
-}
+            <!--main menu -->
+            <div class="side_menu_section">
+                <ul class="menu_nav">
+                    <li >
+                        <a href="index.html">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="about.html">
+                            About Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="services.html">
+                            Services
+                        </a>
+                    </li>
+                    <li>
+                        <a href="portfolio.html">
+                            Portfolio
+                        </a>
+                    </li>
+                    <li>
+                        <a href="blog.html">
+                            Blog
+                        </a>
+                    </li>
+                    <li>
+                        <a href="contact.html">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!--main menu end -->
 
-button:hover {
-	color: #000;
-	border: solid #000 2px;
-  text-decoration: none;
-}
+            <!--filter menu -->
+            <div class="side_menu_section">
+                <h4 class="side_title">filter by:</h4>
+                <ul  id="filtr-container"  class="filter_nav">
+                    <li  data-filter="*" class="active"><a href="javascript:void(0)" >all</a></li>
+                    <li data-filter=".branding"> <a href="javascript:void(0)">branding</a></li>
+                    <li data-filter=".design"><a href="javascript:void(0)">design</a></li>
+                    <li data-filter=".photography"><a href="javascript:void(0)">photography</a></li>
+                    <li data-filter=".architecture"><a href="javascript:void(0)">architecture</a></li>
+                </ul>
+            </div>
+            <!--filter menu end -->
 
-input, textarea {
-	font-size: 0.8rem;
-  border: 1px solid #ddd;
-  background: #fff;
-  padding: 0.5rem 0.7rem;
-	margin: 5px 0;
-  transition: all 0.3s ease-in-out;
-  width: 100%;
 
-}
+            <!--social and copyright -->
+            <div class="side_menu_bottom">
+                <div class="side_menu_bottom_inner">
+                    <ul class="social_menu">
+                        <li>
+                            <a href="#"> <i class="ion ion-social-pinterest"></i> </a>
+                        </li>
+                        <li>
+                            <a href="#"> <i class="ion ion-social-facebook"></i> </a>
+                        </li>
+                        <li>
+                            <a href="#"> <i class="ion ion-social-twitter"></i> </a>
+                        </li>
+                        <li>
+                            <a href="#"> <i class="ion ion-social-dribbble"></i> </a>
+                        </li>
+                    </ul>
+                    <div class="copy_right">
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        <p class="copyright">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </div>
+                </div>
+            </div>
+            <!--social and copyright end -->
 
-input:focus, textarea:focus {
-  border-color: #777;
-  outline: none;
-}
+        </div>
+        <!--=================== side menu end====================-->
 
-.portfolio-content {
-	line-height: 2;
-	color: #555;
-}
+        <!--=================== content body ====================-->
+        <div class="col-lg-10 col-md-9 col-12 body_block  align-content-center">
+            <div>
+                <div class="img_card">
+                    <div class="row justify-content-center">
+                        <div class="col-md-6 col-7 content_section">
+                            <div class="content_box">
+                                <div class="content_box_inner">
+                                    <div>
+                                        <h3>
+                                            Just a few words about us
+                                        </h3>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor. Etiam finibus consequat ante ac congue. Quisque porttitor porttitor tempus. Donec maximus ipsum non ornare vestibulum. Sed libero nibh, feugiat at enim id, bibendum sollicitudin arcu. Aliquam elit nibh, finibus eget mauris non, pretium vehicula ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                        </p>
+                                        <!--=================== counter start====================-->
+                                        <div class="pt50">
+                                            <div class="row justify-content-center">
+                                                <div class="col-12 col-md-4">
+                                                    <div class="counter_box">
+                                                        <div class="divider"></div>
+                                                        <span class="counter">12</span>
+                                                        <h5>Years of experience</h5>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 col-md-4">
+                                                    <div class="counter_box">
+                                                        <div class="divider"></div>
+                                                        <span class="counter">257</span>
+                                                        <h5>happy clients</h5>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 col-md-4">
+                                                    <div class="counter_box">
+                                                        <div class="divider"></div>
+                                                        <span class="counter">192</span>
+                                                        <h5>projects completed</h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--=================== counter end====================-->
+                                    </div>
+                                    <!--===================testimonial start====================-->
+                                    <div class="testimonial_carousel mt50">
 
-.portfolio-content a {
-	color: #333;
-	text-decoration: none;
-	border-bottom: 1px solid #bbb;
-}
+                                        <div class="testimonial_box">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor. Etiam finibus consequat ante ac congue. Quisque porttitor porttitor tempus.
+                                            </p>
+                                            <div class="testimonial_author">
+                                                <img src="assets/img/user.png" alt="author">
+                                                <h5>maria smith</h5>
+                                                <p>project manager <span>company</span></p>
+                                            </div>
+                                        </div>
 
-.portfolio-content a:hover {
-	color: #000;
-	text-decoration: none;
-	border-bottom: 1px solid #555;
-}
+                                        <div class="testimonial_box">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor. Etiam finibus consequat ante ac congue. Quisque porttitor porttitor tempus.
+                                            </p>
+                                            <div class="testimonial_author">
+                                                <img src="assets/img/user.png" alt="author">
+                                                <h5>maria smith</h5>
+                                                <p>project manager <span>company</span></p>
+                                            </div>
+                                        </div>
 
-h1, h2, h3 {
-	font-family: 'Crimson Text', sans-serif;
-	letter-spacing: 1px;
-	font-weight: 300;
-	margin-bottom: 20px;
-}
+                                        <div class="testimonial_box">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor. Etiam finibus consequat ante ac congue. Quisque porttitor porttitor tempus.
+                                            </p>
+                                            <div class="testimonial_author">
+                                                <img src="assets/img/user.png" alt="author">
+                                                <h5>maria smith</h5>
+                                                <p>project manager <span>company</span></p>
+                                            </div>
+                                        </div>
 
-.navbar-brand {
-	font-weight: 800;
-	font-size: 1.5rem;
-	letter-spacing: 0.5px;
-}
+                                        <div class="testimonial_box">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor. Etiam finibus consequat ante ac congue. Quisque porttitor porttitor tempus.
+                                            </p>
+                                            <div class="testimonial_author">
+                                                <img src="assets/img/user.png" alt="author">
+                                                <h5>maria smith</h5>
+                                                <p>project manager <span>company</span></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!--===================testimonial end====================-->
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-5 img_section" style="background-image: url('assets/img/bg/about.png');"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--=================== content body end ====================-->
+    </div>
+</div>
 
-.navbar {
-	text-transform: uppercase;
-}
-
-.nav-item {
-	font-size: 0.8rem;
-	font-weight: 300;
-	letter-spacing: 1.5px;
-}
-
-.navbar-toggler {
-	border: 0;
-}
-
-.navbar-toggler-right {
-	right: 0rem;
-}
-
-.portfolio-img {
-	width: 100%;
-	max-height: 250px;
-	display: block;
-	object-fit: cover;
-}
-
-.project-img {
-	width: 100%;
-	display: block;
-	object-fit: cover;
-}
-
-/* Image Hover Effect */
-
-.hovereffect {
-	width: 100%;
-  height: 100%;
-  float: left;
-  overflow: hidden;
-  position: relative;
-  text-align: center;
-}
-
-.hovereffect .overlay {
-	width: 100%;
-  height: 100%;
-  position: absolute;
-  overflow: hidden;
-  top: 0;
-  left: 0;
-}
-
-.hovereffect .overlay-text {
-	font-size: 1.3rem;
-	font-family: 'Crimson Text', sans-serif;
-	font-weight: 300;
-	color: #000;
-	left: 50%;
-	position: absolute;
-	top: 50%;
-	width: 100%;
-	-webkit-transform: translate(-50%,-50%);
-	-ms-transform: translate(-50%,-50%);
-	transform: translate(-50%,-50%);
-	opacity: 0;
-  filter: alpha(opacity=0);
-  -webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
-  transition: opacity 0.35s, transform 0.35s;
-}
-
-.hovereffect:hover .overlay-text {
-	opacity: 1;
-  filter: alpha(opacity=100);
-}
-
-.hovereffect img {
-  display: block;
-  position: relative;
-  max-width: none;
-	width: 100%;
-	height: 100%;
-  -webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
-  transition: opacity 0.35s, transform 0.35s;
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
-}
-
-.hovereffect:hover img {
-  opacity: 0;
-  filter: alpha(opacity=0);
-}
-.page-nav {
-	width: 50%;
-	margin: 0 auto;
-	text-align: center;
-}
-.page-nav a,span {
-	font-size: 1.3rem;
-	font-family: 'Crimson Text', sans-serif;
-	font-weight: 300;
-	margin: 0 10px;
-	color: #000;
-}
+<!-- jquery -->
+<script src="assets/js/jquery.min.js"></script>
+<!-- bootstrap -->
+<script src="assets/js/popper.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<!--slick carousel -->
+<script src="assets/js/slick.min.js"></script>
+<!--Portfolio Filter-->
+<script src="assets/js/imgloaded.js"></script>
+<script src="assets/js/isotope.js"></script>
+<!-- Magnific-popup -->
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<!--Counter-->
+<script src="assets/js/jquery.counterup.min.js"></script>
+<!-- WOW JS -->
+<script src="assets/js/wow.min.js"></script>
+<!-- Custom js -->
+<script src="assets/js/main.js"></script>
+</body>
+</html>
