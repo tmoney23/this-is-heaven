c80d1d52c8eac1cbbdca681675a173937afd7fe8
tmoney23 <thomasdmoore@email.com>
2018-06-01 17:05:03 -0400

    new links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,255 +1,99 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <!-- ========== Meta Tags ========== -->
-    <meta charset="UTF-8">
-    <meta name="description" content="Cocoon -Portfolio">
-    <meta name="keywords" content="Cocoon , Portfolio">
-    <meta name="author" content="Pharaohlab">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <!-- ========== Title ========== -->
-    <title> Cocoon -Portfolio</title>
-    <!-- ========== Favicon Ico ========== -->
-    <!--<link rel="icon" href="fav.ico">-->
-    <!-- ========== STYLESHEETS ========== -->
-    <!-- Bootstrap CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Fonts Icon CSS -->
-    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
-    <link href="assets/css/et-line.css" rel="stylesheet">
-    <link href="assets/css/ionicons.min.css" rel="stylesheet">
-    <!-- Carousel CSS -->
-    <link href="assets/css/slick.css" rel="stylesheet">
-    <!-- Magnific-popup -->
-    <link rel="stylesheet" href="assets/css/magnific-popup.css">
-    <!-- Animate CSS -->
-    <link rel="stylesheet" href="assets/css/animate.min.css">
-    <!-- Custom styles for this template -->
-    <link href="assets/css/main.css" rel="stylesheet">
+	<title>Themewagon Live Demo | Profile-Free One Page Portfolio Bootstrap Template</title>
+
+	<!-- Mobile Specific -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<link rel="stylesheet" type="text/css" href="http://themewagon.com/wp-content/themes/wagoncore/css/demo.css">
+
+	<style>
+	body {
+	    height: calc(100% - 51px);
+	    box-sizing: content-box;
+	    min-height: 600px;
+	}
+	</style>
 </head>
 <body>
-<div class="loader">
-    <div class="loader-outter"></div>
-    <div class="loader-inner"></div>
-</div>
 
-<div class="body-container container-fluid">
-    <a class="menu-btn" href="javascript:void(0)">
-        <i class="ion ion-grid"></i>
-    </a>
-    <div class="row justify-content-center">
-        <!--=================== side menu ====================-->
-        <div class="col-lg-2 col-md-3 col-12 menu_block">
-
-            <!--logo -->
-            <div class="logo_box">
-                <a href="#">
-                    <img src="assets/img/logo.png" alt="cocoon">
-                </a>
+<div id="switcher">
+		<div class="center">
+            <div class="logo">
+                <a href="http://www.themewagon.com">
+					<img class="logo" src="https://themewagon.com/wp-content/themes/wagoncore/img/logo-demo.png" alt="ThemeWagon">
+				</a>
             </div>
-            <!--logo end-->
 
-            <!--main menu -->
-            <div class="side_menu_section">
-                <ul class="menu_nav">
-                    <li >
-                        <a href="index.html">
-                            Home
-                        </a>
-                    </li>
-                    <li>
-                        <a href="about.html">
-                            About Us
-                        </a>
-                    </li>
-                    <li>
-                        <a href="services.html">
-                            Services
-                        </a>
-                    </li>
-                    <li>
-                        <a href="portfolio.html">
-                            Portfolio
-                        </a>
-                    </li>
-                    <li>
-                        <a href="blog.html">
-                            Blog
-                        </a>
-                    </li>
-                    <li>
-                        <a href="contact.html">
-                            Contact
-                        </a>
+                <ul>
+                    <li id="theme_list">
+                    	<a id="theme_select" href="https://themewagon.com/themes">View All Themes</a>
                     </li>
                 </ul>
-            </div>
-            <!--main menu end -->
+                <div class="responsive">
+                    <a href="#" class="desktop active" title="View Desktop Version"></a>
+                    <a href="#" class="tabletlandscape" title="View Tablet Landscape (1024x768)"></a>
+                    <a href="#" class="tabletportrait" title="View Tablet Portrait (768x1024)"></a>
+                    <a href="#" class="mobilelandscape" title="View Mobile Landscape (480x320)"></a>
+                    <a href="#" class="mobileportrait" title="View Mobile Portrait (320x480)"></a>
+                </div>
 
-            <!--filter menu -->
-            <div class="side_menu_section">
-                <h4 class="side_title">filter by:</h4>
-                <ul  id="filtr-container"  class="filter_nav">
-                    <li  data-filter="*" class="active"><a href="javascript:void(0)" >all</a></li>
-                    <li data-filter=".branding"> <a href="javascript:void(0)">branding</a></li>
-                    <li data-filter=".design"><a href="javascript:void(0)">design</a></li>
-                    <li data-filter=".photography"><a href="javascript:void(0)">photography</a></li>
-                    <li data-filter=".architecture"><a href="javascript:void(0)">architecture</a></li>
+                <ul class="links">
+                    <li class="download">
+                    	<a href="https://themewagon.com/themes/best-quality-free-portfolio-resume-bootstrap-template-download/">
+                    		<img src="http://themewagon.com/wp-content/themes/wagoncore/img/download.png" alt=""/>
+                    		Download
+                    	</a>
+                    </li>
+                    <li class="close">
+                    	<a href="https://technext.github.io/profile/">
+                    		<img src="http://themewagon.com/wp-content/themes/wagoncore/img/cross.png" alt="" />
+                    		Close
+                    	</a>
+                    </li>
                 </ul>
-            </div>
-            <!--filter menu end -->
-
-
-            <!--social and copyright -->
-            <div class="side_menu_bottom">
-                <div class="side_menu_bottom_inner">
-                    <ul class="social_menu">
-                        <li>
-                            <a href="#"> <i class="ion ion-social-pinterest"></i> </a>
-                        </li>
-                        <li>
-                            <a href="#"> <i class="ion ion-social-facebook"></i> </a>
-                        </li>
-                        <li>
-                            <a href="#"> <i class="ion ion-social-twitter"></i> </a>
-                        </li>
-                        <li>
-                            <a href="#"> <i class="ion ion-social-dribbble"></i> </a>
-                        </li>
-                    </ul>
-                    <div class="copy_right">
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        <p class="copyright">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    </div>
-                </div>
-            </div>
-            <!--social and copyright end -->
-
         </div>
-        <!--=================== side menu end====================-->
-
-        <!--=================== content body ====================-->
-        <div class="col-lg-10 col-md-9 col-12 body_block  align-content-center">
-            <div>
-                <div class="img_card">
-                    <div class="row justify-content-center">
-                        <div class="col-md-6 col-7 content_section">
-                            <div class="content_box">
-                                <div class="content_box_inner">
-                                    <div>
-                                        <h3>
-                                            Just a few words about us
-                                        </h3>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor. Etiam finibus consequat ante ac congue. Quisque porttitor porttitor tempus. Donec maximus ipsum non ornare vestibulum. Sed libero nibh, feugiat at enim id, bibendum sollicitudin arcu. Aliquam elit nibh, finibus eget mauris non, pretium vehicula ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-                                        </p>
-                                        <!--=================== counter start====================-->
-                                        <div class="pt50">
-                                            <div class="row justify-content-center">
-                                                <div class="col-12 col-md-4">
-                                                    <div class="counter_box">
-                                                        <div class="divider"></div>
-                                                        <span class="counter">12</span>
-                                                        <h5>Years of experience</h5>
-                                                    </div>
-                                                </div>
-                                                <div class="col-12 col-md-4">
-                                                    <div class="counter_box">
-                                                        <div class="divider"></div>
-                                                        <span class="counter">257</span>
-                                                        <h5>happy clients</h5>
-                                                    </div>
-                                                </div>
-                                                <div class="col-12 col-md-4">
-                                                    <div class="counter_box">
-                                                        <div class="divider"></div>
-                                                        <span class="counter">192</span>
-                                                        <h5>projects completed</h5>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!--=================== counter end====================-->
-                                    </div>
-                                    <!--===================testimonial start====================-->
-                                    <div class="testimonial_carousel mt50">
-
-                                        <div class="testimonial_box">
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor. Etiam finibus consequat ante ac congue. Quisque porttitor porttitor tempus.
-                                            </p>
-                                            <div class="testimonial_author">
-                                                <img src="assets/img/user.png" alt="author">
-                                                <h5>maria smith</h5>
-                                                <p>project manager <span>company</span></p>
-                                            </div>
-                                        </div>
-
-                                        <div class="testimonial_box">
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor. Etiam finibus consequat ante ac congue. Quisque porttitor porttitor tempus.
-                                            </p>
-                                            <div class="testimonial_author">
-                                                <img src="assets/img/user.png" alt="author">
-                                                <h5>maria smith</h5>
-                                                <p>project manager <span>company</span></p>
-                                            </div>
-                                        </div>
-
-                                        <div class="testimonial_box">
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor. Etiam finibus consequat ante ac congue. Quisque porttitor porttitor tempus.
-                                            </p>
-                                            <div class="testimonial_author">
-                                                <img src="assets/img/user.png" alt="author">
-                                                <h5>maria smith</h5>
-                                                <p>project manager <span>company</span></p>
-                                            </div>
-                                        </div>
-
-                                        <div class="testimonial_box">
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor. Etiam finibus consequat ante ac congue. Quisque porttitor porttitor tempus.
-                                            </p>
-                                            <div class="testimonial_author">
-                                                <img src="assets/img/user.png" alt="author">
-                                                <h5>maria smith</h5>
-                                                <p>project manager <span>company</span></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!--===================testimonial end====================-->
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-5 img_section" style="background-image: url('assets/img/bg/about.png');"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--=================== content body end ====================-->
     </div>
-</div>
 
-<!-- jquery -->
-<script src="assets/js/jquery.min.js"></script>
-<!-- bootstrap -->
-<script src="assets/js/popper.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/waypoints.min.js"></script>
-<!--slick carousel -->
-<script src="assets/js/slick.min.js"></script>
-<!--Portfolio Filter-->
-<script src="assets/js/imgloaded.js"></script>
-<script src="assets/js/isotope.js"></script>
-<!-- Magnific-popup -->
-<script src="assets/js/jquery.magnific-popup.min.js"></script>
-<!--Counter-->
-<script src="assets/js/jquery.counterup.min.js"></script>
-<!-- WOW JS -->
-<script src="assets/js/wow.min.js"></script>
-<!-- Custom js -->
-<script src="assets/js/main.js"></script>
-</body>
-</html>
+    <iframe id="iframe" src="https://technext.github.io/profile/" frameborder="0" width="100%"></iframe>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+    <script type="text/javascript">
+    	var IS_IPAD=navigator.userAgent.match(/iPad/i)!=null;
+    	$("#header-bar").hide();
+			clicked="desktop";
+			var t= {
+				desktop:"100%",tabletlandscape:1040,tabletportrait:788,mobilelandscape:500,mobileportrait:340,placebo:0
+			}
+			;
+			jQuery(".responsive a").on("click",function() {
+				var e=jQuery(this);
+				for (device in t) {
+					if(e.hasClass(device)) {
+						clicked=device;
+						jQuery("#iframe").width(t[device]);
+						if(clicked==device) {
+							jQuery(".responsive a").removeClass("active");
+							e.addClass("active")
+						}
+					}
+				}
+				return false
+			}
+			);
+			if(IS_IPAD) {
+				$("#iframe").css("padding-bottom","60px")
+			}
+    </script>
+
+	 	    <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-55162400-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
